c650bde131b76ebf7cb5ce3945be3aad40276f56
Michjosh <117924669+Michjosh@users.noreply.github.com>
2023-04-17 03:17:41 +0100

    Add files via upload
    First commit on Tic-tac-toe game

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react';
+import GameBoard from './GameBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
+
